8a00c6bbacfa4387ec37b7d24e38c7999f1bfa46
yessh <keh1058@gmail.com>
2025-10-23 20:17:38 +0900

    feat(Running): 최종 우승자 이름 List로 반환

--- src/main/java/racingcar/domain/Running.java
+++ src/main/java/racingcar/domain/Running.java
@@ -1,16 +1,36 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Running {
 
-    private final Cars cars = new Cars();
 
-    public void play(int count) {
+    public void moveCar(List<Car> cars, int count) {
 
         while (count-- > 0) {
 
-            for (Car car : cars.getCars()) {
+            for (Car car : cars) {
                 car.move();
             }
         }
+    }
+
+    public List<String> parseWinner(List<Car> cars) {
+
+        List<String> winners = new ArrayList<>();
+        int maxPosition = 0;
+
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+                winners.clear();
+                winners.add(car.getName());
+            } else if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
     }
 }
